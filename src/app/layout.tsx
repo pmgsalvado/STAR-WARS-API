ee48d88e17a93f2ddd4baa0602ac8cddb4eacfd7
salvado <pmgsalvado@protonmail.com>
2025-07-31 14:29:31 +0100

    add Apollo pagination to homepage table fetch data two by two and button to trigger the load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/**
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" 
+          crossOrigin="anonymous"></link>
+           */}
+      </head>
       <body>
         <ApolloProviderWrapper>
           {children}
--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,8 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
 import Link from "next/link";
 import styles from "@/app/page.module.css";
+import { useEffect, useState } from "react";
 
-// query definition
+// query definition  ( not breing used anymore)
 const GET_FILM = gql`
   query {
     allFilms{
@@ -11,38 +12,107 @@ const GET_FILM = gql`
         title
         }
       }
-    }
-`;
-
+}`;
 
 interface FILMS {
   id: string;
   title: string;
 }
 
+// query definition with pagination
+const PAGE_SIZE = 2;
+
+const GET_FILM_SLICE = gql`
+  query ($first: Int!, $after: String){
+    allFilms(first:$first, after: $after){
+      edges {
+        node {
+          id
+          title
+        }
+      cursor
+      }
+      pageInfo{
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`;
+
+interface Film {
+  id: string;
+  title: string;
+}
+
+interface FilmEdge {
+  node: Film;
+  cursor: string;
+}
+
+interface PageInfo {
+  endCursor: string;
+  hasNextPage: boolean;
+}
+
+interface AllFilmsResult {
+  allFilms: {
+    edges: FilmEdge[];
+    pageInfo: PageInfo;
+  }
+}
+
+
+
 // component definition
 export default function FilmList(){
-  const {loading, error, data} = useQuery(GET_FILM);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+  // fetch films two by two Pagination
+  const {data, loading, error, fetchMore} = useQuery<AllFilmsResult>(GET_FILM_SLICE, {
+    variables: {first: PAGE_SIZE},
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const [films, setFilms] = useState<FilmEdge[]>([])
+
+  useEffect(()=>{
+    if (data?.allFilms?.edges) {
+      setFilms(data.allFilms.edges)
+    }
+  }, [data])
+
+  async function handleLoadMore(){
+    const endCursor = data?.allFilms?.pageInfo?.endCursor;
+
+    const { data: moreData } = await fetchMore({
+      variables: {
+        first: PAGE_SIZE,
+        after: endCursor,
+      }, 
+      updateQuery: (prev, {fetchMoreResult}) => {
+        if (!fetchMoreResult) return prev;
+        return {
+          allFilms: {
+            edges: [...prev.allFilms.edges, ...fetchMoreResult.allFilms.edges],
+            pageInfo: fetchMoreResult.allFilms.pageInfo,
+          },
+        }
+      }
+    });
+
+    if(moreData?.allFilms?.edges) {
+      setFilms((prev) => [...prev, ...moreData.allFilms.edges])
+    }
+  };
+
+  if (loading) return <p>Loading....</p>
+  if (error) return <p>Error: {error.message}</p>
+
   
-  
-
-  const filmListElement = (data.allFilms.films as FILMS[]).map(film =>
-    <tr key={film.id} className={styles.trow}>
-      <td>
-        <Link href={`pages/${film.id}`}>
-          {film.title}
-        </Link>
-      </td>
-      
-    </tr>
-    
-    )
-
   return (
     <div>
+
+      {films.length != 0 && (
       <table className={styles.table}>
         <thead>
           <tr>
@@ -51,9 +121,23 @@ export default function FilmList(){
           </tr>
         </thead>
         <tbody>
-          {filmListElement}
+
+          {films.map(({node}) => (
+            <tr key={node.id} className={styles.trow}>
+              <td>
+                <Link href={`pages/${node.id}`}>
+                  {node.title}
+                </Link>
+              </td>
+            </tr>
+          ))
+          }
         </tbody>
-      </table>
+      </table>)}
+
+      {data?.allFilms.pageInfo.hasNextPage && (
+        <button className={styles.loadButton} onClick={handleLoadMore}>Load More</button>
+      )}  
     </div>
   )
 
