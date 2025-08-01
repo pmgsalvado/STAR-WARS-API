6d27417bf3d96f746843db19d5742e8bbec8ae6f
salvado <pmgsalvado@protonmail.com>
2025-08-01 10:09:36 +0100

    cleaned Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,8 @@ import { useQuery, gql } from "@apollo/client";
 import FilmList from "@/components/FilmList"
 
 
-// to erase in the future
-import characterConstant from "../../public/character.json"
 
 export default function Home() {
-
-  //console.log(characterConstant.characters)
-  // Map through characters Array an populale table with character info
-  const charactersElements = characterConstant.characters.map(character => 
-    <tr key={character.id} className={styles.trow}>
-      <td>
-        <Link href={`/pages/${character.id}`}>
-          {character.name}
-        </Link>
-      </td> 
-      <td>
-        <Link href={`/pages/${character.id}`}>
-          {character.gender}
-        </Link>
-      </td>
-      <td>
-        <Link href={`/pages/${character.id}`}>
-          {character.skinColor}z
-        </Link>
-        </td>
-    </tr>
-  )
-
 
   return (
     
